69fa5d5fe083eca0400f9285629d7c984a5b8a1c
ashisht12 <ashisht12@BSPUNL-18187>
2024-02-16 14:59:40 +0530

    Add project files.

---
+++ 20240216-DemoFA/HTTPExample.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace DemoFA
+{
+    public class HTTPExample
+    {
+        private readonly ILogger<HTTPExample> _logger;
+
+        public HTTPExample(ILogger<HTTPExample> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("HTTPExample")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
